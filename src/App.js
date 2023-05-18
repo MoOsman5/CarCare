849daebc456d4d69dae89b0a1d008e4ce624af81
MoOsman5 <mohamed1592000gmail.com>
2023-05-18 21:31:36 +0200

    add components to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import Header from './components/Header';
+import About from './components/about';
+import Portfolio from './components/portfolio';
+import Services from './components/services';
+import Choose from './components/choose';
+import Stat from './components/stat';
+import Team from './components/team';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About />
+      <Portfolio />
+      <Services />
+      <Choose/>
+      <Stat/>
+      <Team/>
+      <Footer/>
     </div>
   );
 }
